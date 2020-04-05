af0a3a1875094f677ee78bebdca4d9c00601efcd
Saman <ebsa491@gmail.com>
2020-04-05 11:57:59 +0430

    install.sh added

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# GNU Lesser General Public License (LGPL) v3.0 .
+# The project has been developed by Saman Ebrahimnezhad .
+# Created at Iran (IR) .
+# Bash .
+
+echo -e "[\033[0;32m*\033[0m] Installing PhoneFinder..."
+
+sudo cp dist/PhoneFinder /bin/
+
+echo
+
+echo -e "\033[0;32m Installed successfully! \033[0m"
