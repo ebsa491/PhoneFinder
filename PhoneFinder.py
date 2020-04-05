d2d4e3d267ec411f10661a7d670debc521b1311d
Saman <ebsa491@gmail.com>
2020-04-05 11:51:25 +0430

    App completed

---
+++ PhoneFinder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# GNU Lesser General Public License (LGPL) v3.0 .
+# The project has been developed by Saman Ebrahimnezhad .
+# Created at Iran (IR) .
+# Python 3 .
+
+import json
+
+class PhoneFinder:
+
+    def __init__(self):
+
+        # figlet used for ASCII
+
+        icon = open('icon.txt', 'r')
+
+        icon_contents = icon.read()
+
+        print(icon_contents)
+
+        icon.close()
+
+        code = input("Enter the code: ")
+
+        f = open("Phone_Codes.json", "r")
+
+        jsonFile = f.read()
+
+        parsedJson = json.loads(jsonFile)
+
+        for x in parsedJson:
+        	if code == x:
+        		print(parsedJson[x])
+        		break
+
+        f.close()
+
+def main():
+    app = PhoneFinder()
+
+if __name__ == '__main__':
+    main()
